b3a4cfe1d01a5221e1c1b4bb8ba4c00081455ab4
Adarsh Dubey <hanuman200427@gmail.com>
2026-05-03 12:14:51 +0530

    EduPlanner backend initial commit

---
+++ models/Task.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String
+  },
+  deadline: {
+    type: Date
+  },
+  status: {
+    type: String,
+    enum: ["Pending", "In Progress", "Completed"],
+    default: "Pending"
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Task", taskSchema, "student_tasks");
